043e15c7df455b610d86e7d733db21c8b3e75725
tangzhankun <zhankun.tang@intel.com>
2015-08-10 12:16:04 +0800

    refactor the scheduler: move the logic to an agent class for testing

--- src/scheduler/CephSchedulerAgent.hpp
+++ src/scheduler/CephSchedulerAgent.hpp
@@ -16,25 +16,30 @@
  * limitations under the License.
  */
 
-#ifndef _CEPHMESOS_SCHEDULER_CEPHSCHEDULER_HPP_
-#define _CEPHMESOS_SCHEDULER_CEPHSCHEDULER_HPP_
+#ifndef _CEPHMESOS_SCHEDULER_CEPHSCHEDULERAGENT_HPP_
+#define _CEPHMESOS_SCHEDULER_CEPHSCHEDULERAGENT_HPP_
 
 #include <mesos/scheduler.hpp>
 #include <mesos/resources.hpp>
 #include <mesos/type_utils.hpp>
 
-#include "CephSchedulerAgent.hpp"
+#include "EventLoop.hpp"
+#include "common/Config.hpp"
+#include "common/TaskType.hpp"
+#include "common/FrameworkMessage.hpp"
+#include "state/StateMachine.hpp"
 
+using ceph::TaskType;
 using std::string;
 using std::vector;
 using namespace mesos;
 
-class CephScheduler : public Scheduler
+class CephSchedulerAgent
 {
 public:
-  CephScheduler(Config* _config);
+  CephSchedulerAgent(Config* _config);
 
-  virtual ~CephScheduler();
+  virtual ~CephSchedulerAgent();
 
   void registered(
       SchedulerDriver* driver,
@@ -68,10 +73,39 @@ public:
   void error(SchedulerDriver* driver, const std::string& message);
 
 private:
+  bool offerNotEnoughResources(const Offer& offer, TaskType taskType);
 
-  CephSchedulerAgent* agent;
+  string getFileServerIP();
+
+  string createExecutor(
+      string executorName,
+      TaskType taskType,
+      int token,
+      int isInitialMonNode,
+      ExecutorInfo& executor);
+
+  void addHostConfig(TaskInfo &taskinfo, TaskType taskType, string hostname);
+
+  string createTaskName(TaskType taskType, int token);
+
+  void handleWaitingOSDTasks(SchedulerDriver* driver);
+
+  bool fetchPendingRESTfulRequest();
+
+  bool hasRole(const Offer& offer, string role);
+
+  void launchNode(
+      SchedulerDriver* driver,
+      const Offer& offer,
+      TaskType taskType,
+      int token,
+      int isInitialMonNode,
+      string& taskId,
+      string& executorId);
+
+  Config* config;
+  StateMachine* stateMachine;
+  EventLoop* eventLoop;
 };
-
-int framework(Config* config);
 
 #endif
